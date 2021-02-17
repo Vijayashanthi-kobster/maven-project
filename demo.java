f929b7edd975113c3250cd199e14fd985058b535
Vijayashanthi-kobster <53177551+Vijayashanthi-kobster@users.noreply.github.com>
2021-02-17 16:39:53 +0530

    2nd committed

--- demo.java
+++ demo.java
@@ -2,5 +2,5 @@ class HelloWorld {
 
 public static void main(String[] args) {    
 System.out.println("Hello, World!");      
-System.out.println("New line added");   
+System.out.println("New line added and edited");   
 }}
